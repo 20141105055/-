c1b46cf04fb42fe291f635f8a45c74634c6e6f3f
卢佳明 <571836353@qq.com>
2015-11-11 20:25:04 +0800

    Initial Commit

---
+++ 动态存储分配/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  动态存储分配
+//
+//  Created by 20141105055ljm on 15/11/11.
+//  Copyright (c) 2015年 20141105055ljm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
